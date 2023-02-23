e3eba2841ff039cda728b6c2072e052befbf2dd2
hao759 <qhao74154@gmail.com>
2023-02-23 07:25:13 +0700

    first

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+   ob_start();
+   echo "Hello1";
+   echo "z";
+//haha
+   $ob1 = ob_get_contents();
+   echo "Tutorials Point1";
+   $ob2 = ob_get_contents();
+   ob_end_clean();
+   var_dump($ob1, $ob2);
+?>
