51924964c3ebe7cdb4f18ab76ee5a68100e5f7ea
hao759 <89727582+hao759@users.noreply.github.com>
2023-02-23 08:55:02 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,11 +2,13 @@
    ob_start();
    echo "Hello1";
    echo "z";
-//branch33333
+//branch333331
 //branch3
    $ob1 = ob_get_contents();
    echo "Tutorials Point1";
    $ob2 = ob_get_contents();
+//2
    ob_end_clean();
    var_dump($ob1, $ob2);
 ?>
+//3
